7e09918c1c4b29f6fedb239183cb769cc45169ef
TeodorWolski <teodorwolski88@gmail.com>
2022-05-06 20:27:45 +0200

    Exporting root styles

---
+++ src/views/Root/Root.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+`;
--- src/views/Root/Root.tsx
+++ src/views/Root/Root.tsx
@@ -1,9 +1,13 @@
+import { Wrapper } from './Root.styles';
 import MainTemplate from 'components/templates/MainTemplate/MainTemplate';
+import styled from 'styled-components';
 
 const Root = () => {
   return (
     <MainTemplate>
-      <h1>Hello World!</h1>
+      <Wrapper>
+        <h1>Hello World!</h1>
+      </Wrapper>
     </MainTemplate>
   );
 };
