851dcd58da5b7e6961311ff95ea80d329ae79aa2
TeodorWolski <teodorwolski88@gmail.com>
2022-05-09 14:10:25 +0200

    Creating get handler

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,1 +1,9 @@
-export const handlers = [];
+import { rest } from 'msw';
+import { data } from 'mocks/data';
+import { MovieProps } from 'types';
+
+export const handlers = [
+  rest.get<MovieProps>('/recommendations', (req, res, ctx) => {
+    res(ctx.status(200), ctx.json({ data }));
+  }),
+];
