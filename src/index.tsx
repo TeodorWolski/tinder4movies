7ef2f71369d2d3a252b62a8ecbb0c0c113abc6de
TeodorWolski <teodorwolski88@gmail.com>
2022-05-11 22:34:44 +0200

    Fix: msw for firefox

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import Root from 'views/Root/Root';
 import { worker } from 'mocks/browser';
 
-worker.start();
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>
-);
+
+worker.start().then(() => {
+  root.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>
+  );
+});
