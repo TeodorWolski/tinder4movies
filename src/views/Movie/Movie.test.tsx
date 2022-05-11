0b613272a697b874f6c80c0309ff4b8af5d02dbd
TeodorWolski <teodorwolski88@gmail.com>
2022-05-11 22:32:21 +0200

    Fixing the destructuring issue

--- src/views/Movie/Movie.test.tsx
+++ src/views/Movie/Movie.test.tsx
@@ -1,3 +1,5 @@
 import { useState } from 'react';
+import axiosMock from 'axios';
 import { renderWithThemeProvider } from 'helpers/renderWithThemeProvider';
 import { fireEvent, screen } from '@testing-library/react';
+import MovieCard from 'components/organisms/MovieCard/MovieCard';
--- src/views/Movie/Movie.tsx
+++ src/views/Movie/Movie.tsx
@@ -13,7 +13,6 @@ const Movie = () => {
   const [movies, setMovies] = useState<Recommendation[]>([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const currentMovie = movies[currentIndex];
-  const { id, name, description, url, image, rating } = currentMovie;
 
   const notify = (isAccepted?: boolean) => {
     if (isAccepted) {
@@ -74,12 +73,12 @@ const Movie = () => {
       <Heading>Czy lubisz ten</Heading>
       {movies.length > 0 && (
         <MovieCard
-          id={id}
-          name={name}
-          description={description}
-          image={image}
-          url={url}
-          rating={rating}
+          id={currentMovie.id}
+          name={currentMovie.name}
+          description={currentMovie.description}
+          image={currentMovie.image}
+          url={currentMovie.url}
+          rating={currentMovie.rating}
         />
       )}
       <ButtonWrapper>
