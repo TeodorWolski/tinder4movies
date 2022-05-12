b07864d7e748cafb4b15d373734c63890248f098
TeodorWolski <teodorwolski88@gmail.com>
2022-05-12 12:03:18 +0200

    Changing button test

--- src/components/atoms/Button/Button.test.tsx
+++ src/components/atoms/Button/Button.test.tsx
@@ -2,13 +2,11 @@ import { screen } from '@testing-library/react';
 import { newRender } from 'helpers/newRender';
 import Button from './Button';
 
-describe('Is button connected to theme provider?', () => {
-  it('renders the button', () => {
+describe('Button', () => {
+  it('Is button connected to theme provider?', () => {
     newRender(<Button />);
   });
-});
 
-describe('Checks if the button is checking the isDecline prop', () => {
   it('checks if renders button with props', () => {
     const isDecline = true;
     newRender(<Button isDecline={isDecline}> Declined button </Button>);
