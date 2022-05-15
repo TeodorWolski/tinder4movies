b9eb3f5ae2743b2a7440fff414191dd909b4ea06
TeodorWolski <teodorwolski88@gmail.com>
2022-05-15 20:22:36 +0200

    adding missing types in the movie provider

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,9 +1,9 @@
-import { rest } from 'msw';
+import { rest, DefaultRequestBody } from 'msw';
 import { recommendations } from 'mocks/data';
 import { Recommendation } from 'types';
 
 export const handlers = [
-  rest.get<Recommendation[]>('/recommendations', (req, res, ctx) => {
+  rest.get('/recommendations', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ recommendations }));
   }),
 
